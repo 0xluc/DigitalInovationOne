b61cdcbffe681ab5fecf62b799ed254881810079
silverscalp <lucasbrodrigues14@gmail.com>
2022-07-29 17:40:29 -0300

    dotnet

---
+++ BootcampDIOUnimed/dotnet/fundamentos-de-colecoes-e-linq-com-net/Colecoes/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[,] matriz = new int[4,2]
+{
+    { 8,8 },
+    { 8,8 },
+    { 8,8 },
+    { 8,8 }
+};
+for (int a = 0; a < matriz.GetLength(0); a++)
+{
+    for (int b = 0; b < matriz.GetLength(1); b++)
+    {
+        System.Console.WriteLine(matriz[a,b]);
+    }
+}
